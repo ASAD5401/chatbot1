624a44784adcb5cd989cac85936b0be9934a1f04
unknown <asadali5401@gmail.com>
2021-10-06 21:39:03 +0500

    fghjk

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var x=Number(document.getElementById('num1').value)
+    var y=Number(document.getElementById('num2').value)
+    var res=x+y
+    var result=document.getElementById('result')
+    result.innerHTML=res
+
+}
